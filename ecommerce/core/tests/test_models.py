7a9b599a1a9946e48be93488e40bf489ed5d4db9
zubair-arbi <zubair.afzal@arbisoft.com>
2016-06-08 20:09:23 +0500

    fix failing tests (part 1)

--- ecommerce/core/tests/test_models.py
+++ ecommerce/core/tests/test_models.py
@@ -160,9 +160,11 @@ class SiteConfigurationTests(TestCase):
         'ecommerce.extensions.payment.tests.processors.AnotherDummyProcessor',
     ])
     @ddt.data(
-        ([], []),
-        ([DummyProcessor], [DummyProcessor]),
-        ([DummyProcessor, AnotherDummyProcessor], [DummyProcessor, AnotherDummyProcessor]),
+        ([], {}),
+        ([DummyProcessor], {DummyProcessor.Name: DummyProcessor}),
+        ([DummyProcessor, AnotherDummyProcessor], {
+            DummyProcessor.Name: DummyProcessor, AnotherDummyProcessor.Name: AnotherDummyProcessor
+        }),
     )
     @ddt.unpack
     def test_get_payment_processors(self, processors, expected_result):
@@ -198,7 +200,7 @@ class SiteConfigurationTests(TestCase):
     )
     def test_get_payment_processors_switch_disabled(self, processors):
         """ Tests that get_payment_processors respects waffle switches """
-        expected_result = []
+        expected_result = {}
         site_config = _make_site_config(",".join(proc.NAME for proc in processors))
 
         result = site_config.get_payment_processors()
